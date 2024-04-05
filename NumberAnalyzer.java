4d22730526dc52284d269dcf4e73f02113796723
varivinayreddy <128284140+varivinayreddy@users.noreply.github.com>
2024-04-05 13:16:18 +0530

    Create NumberAnalyzer.java

---
+++ NumberAnalyzer.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class NumberAnalyzer {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int largest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
+        int sum = 0;
+        int count = 0;
+
+        char choice;
+        do {
+            System.out.print("Enter a number: ");
+            int number = scanner.nextInt();
+
+            sum += number;
+            count++;
+
+            if (number > largest) {
+                largest = number;
+            }
+
+            if (number < smallest) {
+                smallest = number;
+            }
+
+            System.out.print("Do you want to enter another number? (y/n): ");
+            choice = scanner.next().charAt(0);
+        } while (choice == 'y');
+
+        if (count > 0) {
+            double average = (double) sum / count;
+
+            System.out.println("Largest number entered: " + largest);
+            System.out.println("Smallest number entered: " + smallest);
+            System.out.println("Average of all numbers entered: " + average);
+        } else {
+            System.out.println("No numbers were entered.");
+        }
+
+        scanner.close();
+    }
+}
